4963f2e5aedf9fa9b641f2ba9f6d4d955aa596a5
Giovannif17 <103296186+Giovannif17@users.noreply.github.com>
2022-07-15 16:20:54 -0400

    Add files via upload

---
+++ BankingProject/FirstBank.java
@@ -0,0 +1,15 @@
+public class FirstBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Account Gio = new Account("Gio", "A01010");
+		
+		
+		Account Giovanni = new Account("Giovanni", "G01010");
+		Giovanni.showMenu();
+		
+		
+	}
+
+}
